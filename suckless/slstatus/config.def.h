4f31fe02e6dc4e05b86d4ba3a6f44aa2895e7d30
Peglah <peglah@gmail.com>
2022-06-08 10:40:14 +0200

    adjusted slstatus

--- suckless/slstatus/config.def.h
+++ suckless/slstatus/config.def.h
@@ -65,10 +65,15 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ netspeed_rx, "  %s ",     "enp1s0" },
+	{ netspeed_rx, "  %s ",    "enp1s0" },
 	{ netspeed_tx, " %s | ",   "enp1s0" },
-  { load_avg, "%s | ",             NULL },
-  { uptime, "%s | ",             NULL },
+	//{ netspeed_rx, "  %s ",    "wlp3s0" },
+	//{ netspeed_tx, " %s | ",   "wlp3s0" },
+  { load_avg, "%s | ",         NULL },
+  //{ uptime, "Up %s | ",        NULL },
+  //{ battery_state, "%s",     "BAT0" },
+  //{ battery_perc, "%s%% ",      "BAT0" },
+  //{ battery_remaining, "%s | ", "BAT0" },
 	{ datetime, "%s",           "%F %R" },
 };
 
