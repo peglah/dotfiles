f3a71ff19a497dce4de0dd229cecb56329d9af35
Peglah <peglah@gmail.com>
2022-08-15 14:04:08 +0200

    small fixes

--- suckless/dwm-6.3/config.def.h
+++ suckless/dwm-6.3/config.def.h
@@ -17,7 +17,7 @@ static const char col_orange[]      = "#fe8019"; // sel bg and border
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_orange  },
+	[SchemeSel]  = { col_gray4, col_gray2,  col_orange  },
 };
 
 /* tagging */
