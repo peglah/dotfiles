fe5327b11a39e48a0039d4be9e1c58ba610e3f3f
Peglah <peglah@gmail.com>
2023-01-17 11:42:42 +0100

    fixed os dependent config

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,15 +1,12 @@
 local vim = vim
 require('plugins')
 
--- aTODO --
---print(vim.loop.os_uname().sysname)
-
---if(vim.fn.has('windows')){
---Windows specific config here
---}
-
--- Bindings --
---api.nvim_set_keymap("n", "<leader>gs", ":Git<CR>", {noremap = true})
+-- TODO --
+--if vim.loop.os_uname().sysname == 'Windows_NT' then
+--  print('Windows_NT')
+--elseif vim.loop.os_uname().sysname == 'Linux' then
+--  print('Linux')
+--end
 
 -- Config --
 -- Set language
@@ -74,23 +71,17 @@ vim.cmd([[ autocmd BufWritePre *.[ch] %s/\%$/\r/e ]], false)
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
   --  ensure_installed = { "c", "lua", "rust" },
-  ensure_installed = { "c", "cpp", "help", "lua", "vim" },
+  ensure_installed = { "help", "lua", "vim" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  sync_install = true,
 
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
 
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    --disable = { "c", "rust" },
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
