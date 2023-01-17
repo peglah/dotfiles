54d7ab354a276842369b4382112950f64b49d93c
Peglah <peglah@gmail.com>
2023-01-17 09:56:36 +0100

    updated bootstrap

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,22 +1,15 @@
-local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
-vim.api.nvim_create_autocmd(
-  "BufWritePost",
-  { command = "source <afile> | PackerCompile", group = packer_group, pattern = "init.lua" }
-)
-
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
-vim.api.nvim_command("packadd packer.nvim")
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup({
   function(use)
@@ -27,52 +20,76 @@ return require('packer').startup({
     use 'ellisonleao/gruvbox.nvim' -- Theme
       vim.cmd("colorscheme gruvbox")
     use { 'karb94/neoscroll.nvim', -- Smooth scrolling
-      config = function() require('neoscroll').setup() end }
+    config = function() require('neoscroll').setup() end }
     use { 'nvim-lualine/lualine.nvim', -- Status/tabline
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-      config = function() require('lualine').setup() end }
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = function() require('lualine').setup() end }
 
     -- Distraction-free editing
     use { 'folke/zen-mode.nvim', -- Distraction-free coding
-      config = function() require('zen-mode').setup() end }
+    config = function() require('zen-mode').setup() end }
     use { 'folke/twilight.nvim', -- Dims inactive portions of the code you're editing
-      config = function() require('twilight').setup() end }
+    config = function() require('twilight').setup() end }
 
     -- Editing supports
     use { 'p00f/nvim-ts-rainbow', -- Colorful parentheses
-      branch = 'master' }
+    branch = 'master' }
     use { 'norcalli/nvim-colorizer.lua', -- Color highlighter
-      config = function() require('colorizer').setup() end }
+    config = function() require('colorizer').setup() end }
     use 'nvim-treesitter/nvim-treesitter'
 
     --  Utility
     use { 'sudormrfbin/cheatsheet.nvim', -- Hit <leader>? to invoke cheatsheet telescope
-      requires = {
-        { 'nvim-telescope/telescope.nvim' },
-        { 'nvim-lua/popup.nvim' },
-        { 'nvim-lua/plenary.nvim' }, } }
-    use { 'tversteeg/registers.nvim', -- Press " in normal or visual mode or Ctrl-R in insert mode
+    requires = {
+      { 'nvim-telescope/telescope.nvim' },
+      { 'nvim-lua/popup.nvim' },
+      { 'nvim-lua/plenary.nvim' }, } }
+      use { 'tversteeg/registers.nvim', -- Press " in normal or visual mode or Ctrl-R in insert mode
       config = function() require('registers').setup() end }
 
-    --  use 'kshenoy/vim-signature'         -- Place, toggle and display marks
+      --  use 'kshenoy/vim-signature'         -- Place, toggle and display marks
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-      require("packer").sync()
-    end
-  end,
-  config = {
-    -- Using a floating window
-    display = {
-      open_fn = function()
-        return require('packer.util').float({ border = 'single' })
+      -- LSP
+      use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},
+          {'williamboman/mason.nvim'},
+          {'williamboman/mason-lspconfig.nvim'},
+
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},
+          {'hrsh7th/cmp-buffer'},
+          {'hrsh7th/cmp-path'},
+          {'saadparwaiz1/cmp_luasnip'},
+          {'hrsh7th/cmp-nvim-lsp'},
+          {'hrsh7th/cmp-nvim-lua'},
+
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},
+          -- Snippet Collection (Optional)
+          {'rafamadriz/friendly-snippets'},
+        }
+      }
+
+      -- Automatically set up your configuration after cloning packer.nvim
+      -- Put this at the end after all plugins
+      if packer_bootstrap then
+        require("packer").sync()
       end
+    end,
+    config = {
+      -- Using a floating window
+      display = {
+        open_fn = function()
+          return require('packer.util').float({ border = 'single' })
+        end
+      },
+      -- Profile the time taken loading your plugins
+      profile = {
+        enable = true,
+        threshold = 1, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+      },
     },
-    -- Profile the time taken loading your plugins
-    profile = {
-      enable = true,
-      threshold = 1, -- the amount in ms that a plugins load time must be over for it to be included in the profile
-    },
-  },
-})
+  })
