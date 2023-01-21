d745636594ec8007a2eb5dce9c5b82612bb0edf8
Peglah <peglah@gmail.com>
2023-01-21 11:52:42 +0100

    lualine fix

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,8 +2,10 @@ return {
   'nvim-lualine/lualine.nvim', -- Status/tabline
   dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true },
   config = function() require('lualine').setup({
-    sections = { lualine_c = {} },
-    inactive_section = { lualine_c = {} },
+    sections = {
+      lualine_c = {},
+      lualine_y = {}
+    },
     winbar = {
       lualine_a = {
         {
@@ -13,13 +15,14 @@ return {
           max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component
         }
       },
-      lualine_z = {
+      lualine_y = {
         {
           require("lazy.status").updates,
           cond = require("lazy.status").has_updates,
-          color = { fg = "#ff9e64" },
+          color = { fg = "#FE8019" },
         }
-      }
+      },
+      lualine_z = { 'progress' }
     },
   }) end
 }
