c6a782615063f2e8a949813f3dc59167fe591449
Peglah <peglah@gmail.com>
2023-02-13 00:08:49 +0100

    added themes

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,41 +1,8 @@
--- Learn the keybindings, see :help lsp-zero-keybindings
--- Learn to configure LSP servers, see :help lsp-zero-api-showcase
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
-
--- Required options for PS
-require("lspconfig").powershell_es.setup{
-  shell = "powershell.exe",
-  bundle_path = vim.fn.stdpath("data") .. "/mason/packages/PowerShellEditorServices/",
-}
-
--- Fix Undefined global 'vim'
---lsp.configure('sumneko_lua', {
-lsp.configure('lua_ls', {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
-      }
-    }
-  }
+local lsp = require('lsp-zero').preset({
+  name = 'recommended',
 })
 
--- These are the options the recommended preset uses
-lsp.set_preferences({
-  suggest_lsp_servers = true,
-  setup_servers_on_start = true,
-  set_lsp_keymaps = true,
-  configure_diagnostics = true,
-  cmp_capabilities = true,
-  manage_nvim_cmp = true,
-  call_servers = 'local',
-  sign_icons = {
-    error = '✘',
-    warn = '▲',
-    hint = '⚑',
-    info = ''
-  }
-})
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
 
 lsp.setup()
