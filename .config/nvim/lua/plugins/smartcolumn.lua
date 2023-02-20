47bae222e35706a5201e5555182438d3f97347e8
Peglah <peglah@gmail.com>
2023-02-20 22:42:22 +0100

    test smartcolumn

---
+++ .config/nvim/lua/plugins/smartcolumn.lua
@@ -0,0 +1,9 @@
+return {
+  dir = "~/git/smartcolumn.nvim",
+  enabled = false,
+  opts = {
+    colorcolumn = 80,
+    disabled_filetypes = { "help", "text", "markdown" },
+    limit_to_window = false,
+  }
+}
