b6c693c96979fb1f131f6251b903f02c22650b68
Peglah <peglah@gmail.com>
2023-02-22 11:16:07 +0100

    lazy copilot & treesitter

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
-  event = "VimEnter",
+  event = "VeryLazy",
   config = function()
     vim.defer_fn(function()
       require("copilot").setup({
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  event = 'VeryLazy',
+
   config = function() require 'nvim-treesitter.configs'.setup({
     -- A list of parser names, or "all"
     --  ensure_installed = { "c", "lua", "rust" },
