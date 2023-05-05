3176c59d5d169fa75c789c2aec4eae730177e793
Peglah <peglah@gmail.com>
2023-05-05 12:27:26 +0200

    chore

--- .config/nvim/lua/plugins/lsp_lines.lua
+++ .config/nvim/lua/plugins/lsp_lines.lua
@@ -1,9 +1,9 @@
 return {
-  --url = "git@git.sr.ht:~whynothugo/lsp_lines.nvim",
   url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
 
   init = function()
     vim.diagnostic.config { virtual_text = false }
   end,
+
   opts = {}
 }
