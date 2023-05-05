fed47454d2844d4a8cc99f902604fb7831360b4f
Peglah <peglah@gmail.com>
2023-05-05 15:15:35 +0200

    added (auto)keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,6 +7,15 @@ vim.keymap.set('n', '<C-H>', '<C-W><C-H>')
 -- Esc to exit terminal mode
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
+-- Change to Normal mode by pressing "jj" in quick succession
+vim.keymap.set("i", "jj", "<esc>", { desc = "Change to Normal mode" })
+
+-- Resize windows using <Ctrl + Arrow keys>
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Increase window width" })
+
 -- Save and close buffer
 vim.api.nvim_create_user_command('Wbd', 'write <bar> bdelete', {})
 -- Save and source file
