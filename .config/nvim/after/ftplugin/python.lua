1ac58ce775e1ed51d727a8239c0d695ea5159e90
Peglah <peglah@gmail.com>
2023-05-07 22:24:56 +0200

    added python conf

---
+++ .config/nvim/after/ftplugin/python.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "<leader>r", ":write<CR>:terminal python %<CR>", { desc = "Save and run Python" })
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -27,3 +27,11 @@ autocmd({ "VimResized" }, {
     vim.cmd("tabdo wincmd =")
   end,
 })
+
+-- Disable semantic highlight
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+})
