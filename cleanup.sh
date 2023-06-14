ff2e2a9f894ea3a865dccfc746f29144cfd54616
Peglah <peglah@gmail.com>
2023-06-14 14:45:02 +0200

    removed cleanup and moved wt.sh

--- cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# suckless
-rm suckless/dmenu*/config.h suckless/dmenu*/*.o suckless/dmenu*/dmenu suckless/dmenu*/stest
-rm suckless/dwm*/config.h suckless/dwm*/*.o suckless/dwm*/dwm
-rm suckless/st*/config.h suckless/st*/*.o suckless/st*/st
-rm suckless/slstatus/config.h suckless/slstatus/*.o suckless/slstatus/slstatus suckless/slstatus/components/*.o
