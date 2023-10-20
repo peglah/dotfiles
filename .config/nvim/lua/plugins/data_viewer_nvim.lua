02dba5f316cc8611cffde773200e6f290c26e0d9
Peglah <peglah@gmail.com>
2023-10-20 13:16:01 +0200

    added dataviewer plugin

---
+++ .config/nvim/lua/plugins/data_viewer_nvim.lua
@@ -0,0 +1,19 @@
+return {
+  'vidocqh/data-viewer.nvim',
+  dependencies = { "nvim-lua/plenary.nvim" },
+
+  opts={
+--    autoDisplayWhenOpenFile = false,
+--    columnColorEnable = true,
+--    columnColorRoulette = { -- Highlight groups
+--      "DataViewerColumn0",
+--      "DataViewerColumn1",
+--      "DataViewerColumn2",
+--    },
+--    view = {
+--      width = 0.8, -- Less than 1 means ratio to screen width
+--      height = 0.8, -- Less than 1 means ratio to screen height
+--      zindex = 50,
+--    }
+  }
+}
