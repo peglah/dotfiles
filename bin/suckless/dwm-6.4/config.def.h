06ac64988009cf5fac9066bb0be84cbf018e403f
Peglah <peglah@gmail.com>
2023-11-23 14:04:11 +0100

    added wtwitch binding

--- bin/suckless/dwm-6.4/config.def.h
+++ bin/suckless/dwm-6.4/config.def.h
@@ -67,16 +67,17 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-//static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-l", "6", "-g", "3", "-bw", "2", "-c", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *firefoxcmd[]  = { "firefox", NULL };
+static const char *wtwitchcmd[] = { "sh", "-c", "wtwitch c | grep -oP '^\\s+\\K\\S+(?=:)'" " | grep -v 'Settings' | sed 's/\\x1B\\[[0-9;]\\{1,\\}[A-Za-z]//g' | dmenu -fn 'FiraCode Nerd Font Mono:style=Retina,Regular:pixelsize=16:antialias=true:autohint=true' -l 6 -g 3 -bw 2 -c | xargs -r wtwitch w", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = firefoxcmd } },
+  { MODKEY,                       XK_c,      spawn,          {.v = wtwitchcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
