946b4e4ee258fe56996bd30e404d4bb5b13e84ef
Peglah <peglah@gmail.com>
2024-04-16 13:58:25 +0200

    updated config

--- .config/nvim/lua/plugins/markdown_preview_nvim.lua
+++ .config/nvim/lua/plugins/markdown_preview_nvim.lua
@@ -1,7 +1,7 @@
 return {
   "iamcco/markdown-preview.nvim",
-  ft = "markdown",
-
-  build = ":call mkdp#util#install()",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function() vim.fn["mkdp#util#install"]() end,
 }
 
