4ce662ed830f2f5fd67926b2c9b8e0587b015c02
Peglah <peglah@gmail.com>
2024-05-03 22:41:23 +0200

    changed logo

--- bin/suckless/slock-1.5/config.def.h
+++ bin/suckless/slock-1.5/config.def.h
@@ -12,25 +12,25 @@ static const char *colorname[NUMCOLS] = {
 static const int failonclear = 1;
 
 /* time in seconds before the monitor shuts down */
-static const int monitortime = 5;
+static const int monitortime = 60;
 
 /* insert grid pattern with scale 1:1, the size can be changed with logosize */
 static const int logosize = 75;
 /* grid width and height for right center alignment */
-static const int logow = 12;
-static const int logoh = 6;
+static const int logow = 6;
+static const int logoh = 12;
 
 static XRectangle rectangles[9] = {
 	/* x    y       w       h */
-	{ 0,    3,      1,      3 },
-	{ 1,    3,      2,      1 },
-	{ 0,    5,      8,      1 },
-	{ 3,    0,      1,      5 },
-	{ 5,    3,      1,      2 },
-	{ 7,    3,      1,      2 },
-	{ 8,    3,      4,      1 },
-	{ 9,    4,      1,      2 },
-	{ 11,   4,      1,      2 },
+  { 0,    1,      1,      9 },
+  { 1,   10,      1,      3 },
+  { 1,    4,      5,      1 },
+  { 2,    8,      4,      1 },
+  { 2,   10,      3,      1 },
+  { 5,   10,      1,      3 },
+  { 6,    0,      1,     10 },
+  { 2,    5,      1,      2 },
+  { 4,    5,      1,      2 },
 };
 
 /*Enable blur*/
