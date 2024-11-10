25cf31377f50779da0c0b7fb679b59f23ba19507
Peglah <peglah@gmail.com>
2024-11-10 08:27:10 +0100

    run in bg

--- .config/tmux/check_dotfiles.sh
+++ .config/tmux/check_dotfiles.sh
@@ -4,14 +4,22 @@ REPO_DIR="$HOME/git/Peglah/dotfiles"
 
 cd "$REPO_DIR" || exit
 
-# Update the local repository
-git fetch origin
-LOCAL_HASH=$(git rev-parse HEAD)
-REMOTE_HASH=$(git rev-parse origin/main)
+# Run git fetch with a timeout in the background
+timeout 10 git fetch origin &
+FETCH_PID=$!
 
-if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
-  tmux popup -w27 -h4 "echo 'Dotfiles update available'"
+# Wait for the fetch process to complete (up to 10 seconds)
+wait $FETCH_PID
+FETCH_EXIT_CODE=$?
+
+# Check if the fetch was successful (exit code 0 means success)
+if [ $FETCH_EXIT_CODE -eq 0 ]; then
+  LOCAL_HASH=$(git rev-parse HEAD)
+  REMOTE_HASH=$(git rev-parse origin/main)
+
+  if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
+    tmux popup -w27 -h4 "echo 'Dotfiles update available'"
+  fi
 #else
-#  tmux popup -w27 -h4 "echo 'Dotfiles are up to date'"
+#  echo "git fetch failed or timed out"
 fi
-
