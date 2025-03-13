b1022aa4b293d513d12d29deea8c81a77df9d0a2
Peglah <peglah@gmail.com>
2025-03-13 15:06:26 +0100

    add eza

---
+++ setup/debian/scripts/build/eza.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo nala install -y gpg
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo nala update
+sudo nala install -y eza
