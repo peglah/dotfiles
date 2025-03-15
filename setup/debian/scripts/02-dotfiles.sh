98c1296cad6bf9df3afb24d5c734a9c79f4b50d0
Peglah <peglah@gmail.com>
2025-03-15 23:07:29 +0100

    add check

--- setup/debian/scripts/02-dotfiles.sh
+++ setup/debian/scripts/02-dotfiles.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-
 set -euo pipefail
 
 sudo nala install -y stow
 
-shopt -s dotglob
-mv $HOME/.config/* $HOME/git/Peglah/dotfiles/.config/
-rmdir $HOME/.config
+# Ensure ~/.config exists before moving files
+if [[ -d "$HOME/.config" && -n "$(ls -A "$HOME/.config")" ]]; then
+    shopt -s dotglob  # Include hidden files
+    mv "$HOME/.config/"* "$HOME/git/Peglah/dotfiles/.config/"
+    rmdir "$HOME/.config"
+else
+    echo "Warning: ~/.config is empty or does not exist. Skipping move."
+fi
 
-cd $HOME/git/Peglah/dotfiles
-stow . -t $HOME
-ln -s $HOME/git/Peglah/dotfiles/.local/bin/ $HOME/.local/bin
+cd "$HOME/git/Peglah/dotfiles"
+stow . -t "$HOME"
+
+ln -s "$HOME/git/Peglah/dotfiles/.local/bin/" "$HOME/.local/bin"
+
 cd -
 
-mv $HOME/.bash_profile $HOME/.bashrc
-source $HOME/.profile
+mv "$HOME/.bash_profile" "$HOME/.bashrc"
+
+source "$HOME/.profile"
